431a972dfe79845ff1e008a474a3c2253390516f
Héctor Alfonso <147531020+A01563545@users.noreply.github.com>
2025-03-28 16:01:32 +0000

    Primer cambio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
+// importar cada una de las imagenes y les asignamos un objeto
+import MarioImage from './imagenes/Mario.png'
+import LuigiImage from './imagenes/Luigi.png'
+import PeachesImage from './imagenes/Peaches.png'
+
 export function App() {
   return (
     <div>
-        <div>Personajes de Mario Bros</div>
-        <Tarjeta/>
-        <Tarjeta/>
-        <Tarjeta/>
+        <div>Personajes de Mario Bros que mas me gustan</div>
+        <Tarjeta nombre = "Mario" colorFavorito ="Rojo" image={MarioImage}/>
+        <Tarjeta nombre = "Luigi" colorFavorito = "Verde" image={LuigiImage}/>
+        <Tarjeta nombre = "Peaches" colorFavorito = "Rosa" image={PeachesImage}/>
     </div>
   )
 }
